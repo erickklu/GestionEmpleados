6ceb64179ffb6acade7bc370587e7635234b6db5
erick <ericktrejojohanlucio@gmail.com>
2024-06-05 15:37:46 -0500

    Eliminación de carpeta repetida

--- GestionEmpleados/src/controllers/Departamento.js
+++
@@ -1,7 +0,0 @@
-const db = require("../db/models")
-const model = require("../db/models/departamento")(db.sequelize, db.Sequelize)
-
-module.exports.GetDepartamentos = async () => {
-    const response = await model.findAll();
-    return response;
-}
