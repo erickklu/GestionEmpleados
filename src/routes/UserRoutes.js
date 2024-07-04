4c0f4114ff67a389e16e0f19b52ff74bbf8cc851
TREJO LUCIO ERICK JOHAN <EJTREJO@PUCESD.EDU.EC>
2024-07-03 20:50:18 -0500

    Modificacion del login con JWT

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const UserController = require("../controllers/UserController")
+const jwt = require('jsonwebtoken');
 
 
 router.post("/register", async (req, res) => {
@@ -11,10 +12,24 @@ router.post("/register", async (req, res) => {
         res.status(201).send(`Usuario ${name} registrado correctamente. ID: ${response.id}`);
 });
 
-router.post("/login", async (req, res) => {
+/* router.post("/login", async (req, res) => {
     const { username, password } = req.body;
     let user = await UserController.FindUserByEmail({username,password})
     res.status(201).send(user)
+}); */
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    let pass = await UserController.FindUserByEmail({username,password})
+
+    if (pass) {
+        const token = jwt.sign({ username }, process.env.JWT_TOKEN_SECRET, { expiresIn: '1h' })
+        res.json({token})
+    } else {
+        res.status(401).send('Usuario o contraseña incorrectos')
+    }
+
+    /* res.status(201).send(user) */
 });
 
 
